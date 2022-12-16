abfef0eb742437a1c6a1610ed31868d0951cdcca
Braeden Callahan <you@example.com>
2022-12-16 13:20:36 -0800

    removed action trait

--- src/hundred_days/action.rs
+++ src/hundred_days/action.rs
@@ -8,11 +8,3 @@ pub trait Information {
     fn name(&self) -> &str;
     fn description(&self) -> String;
 }
-
-pub trait ItemAction {
-    fn activate(&self, item: String, game: &mut Game, amount: i32) -> String;
-}
-
-pub trait ItemlessAction {
-    fn activate(&self, game: &mut Game, amount: i32) -> String;
-}
